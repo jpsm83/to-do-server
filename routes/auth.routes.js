879f81ca9f41c120a4e5d156ddfd6c9a61ea380a
jpsm83 <jpsm83@hotmail.com>
2021-09-21 20:17:18 +0200

    auth-routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,3 +1,5 @@
+// authentication routes login, signup, logout, edit, loggedin
+
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
@@ -11,7 +13,9 @@ const bcryptSalt = 10;
 router.post('/signup', (req, res, next) => {
   const { username, email, password } = req.body;
 
+  // validators have to be equal to validators from frontend
   if(password.length < 3){
+    // error 400 - bad request
     return res.status(400).json({ message: 'Please make your password at least 3 characters long'});
   }
 
@@ -30,7 +34,8 @@ router.post('/signup', (req, res, next) => {
 
     User.create({
       username, 
-      email, 
+      email,
+      // encrypted password
       password: hashPass
     })
     .then((newUser) => {
@@ -43,6 +48,7 @@ router.post('/signup', (req, res, next) => {
         return res.status(200).json(newUser);
       })
     })
+    // error 500 - server error
     .catch(error => res.status(500).json(error))
   })
 })
@@ -54,6 +60,8 @@ router.post('/login', (req, res, next) => {
     }
 
     if(!theUser){
+      // error message (failureDetails) comes from passport.config
+      // error 401 - unauthorized
       return res.status(401).json(failureDetails);
     }
 
@@ -61,19 +69,21 @@ router.post('/login', (req, res, next) => {
       if(error){
         return res.status(500).json(error);
       }
-
       return res.status(200).json(theUser);
     })
-
-  })(req, res, next)
+  })
+  // in this line, we are calling the function we just created for login
+  (req, res, next)
 })
 
 router.post('/logout', (req, res, next) => {
-  // req.logout is defined by passport
+  // req.logout is a function defined by passport
+  // replaces req.session.destroy
   req.logout();
-  return res.status(200).json({ message: 'Log out success!'});
+  return res.status(200).json({ message: 'Logout success!'});
 })
 
+// cloudnary router for upload photo???
 router.put('/edit', uploader.single('photo'), (req, res, next) => {
   console.log(req.file);
   User.findOneAndUpdate({ _id: req.user.id }, { ...req.body, photo: req.file ? req.file.path : req.user.photo }, { new: true })
@@ -86,6 +96,7 @@ router.get('/loggedin', (req, res, next) => {
   if(req.isAuthenticated()){
     return res.status(200).json(req.user);
   } else {
+    // error 403 - forbidden
     return res.status(403).json({ message: 'Forbbiden' });
   }
 })
